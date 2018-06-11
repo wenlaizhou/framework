02b9f2fbd20a9e2ff4905d50e3ce6c772885ed4b
zhouwenlai <zhouwenlai@chehejia.com>
2018-06-11 11:29:12 +0800

    fucking

--- framework/schedule.go
+++ framework/schedule.go
@@ -6,11 +6,12 @@ import "log"
 var schedule []map[string]func()
 
 func Schedule(name string, timeSchedule int, fun func()) {
+	log.Printf("注册定时任务: %v 间隔时间: %v", name, timeSchedule)
 
 	go func(timeSchedule int, fun func()) {
 		for {
 			time.Sleep(time.Second * time.Duration(timeSchedule))
-			log.Printf("执行定时任务:%v, 时间间隔:%v", name, timeSchedule)
+			log.Printf("执行定时任务: %v", name)
 			fun()
 		}
 	}(timeSchedule, fun)
