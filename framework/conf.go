34cf51a5eb1b9312ba96f7279f887f6c684bbe15
zhouwenlai <zhouwenlai@chehejia.com>
2018-07-04 15:28:50 +0800

    优秀, 突出

--- framework/conf.go
+++ framework/conf.go
@@ -42,7 +42,7 @@ const ConfDir = "CONF_DIR"
 
 type Config map[string]interface{}
 
-func LoadConfig(confPath string) *Config {
+func LoadConfig(confPath string) Config {
 	res := make(Config)
 	if !Exists(confPath) {
 		return nil
@@ -53,5 +53,5 @@ func LoadConfig(confPath string) *Config {
 	}
 	json.Unmarshal(data, &res)
 	res[ConfDir] = filepath.Dir(confPath)
-	return &res
+	return res
 }
