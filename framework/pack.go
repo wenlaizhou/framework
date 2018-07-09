ddedb7c86aa917b96768df9e363a143936d5dcf6
zhouwenlai <zhouwenlai@chehejia.com>
2018-07-09 14:41:28 +0800

    优秀, 突出 可刷新配置

--- framework/pack.go
+++ framework/pack.go
@@ -1,4 +1,17 @@
 package framework
 
-func packDir() {
+import (
+	"os"
+	"path/filepath"
+)
+
+func packDir(root string) {
+	//静态文件编译到变量中
+	//模板文件编译到变量中
+	//将模板文本注册到全局数据体中
+
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+
+		return nil
+	})
 }
